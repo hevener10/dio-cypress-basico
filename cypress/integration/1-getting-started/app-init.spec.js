e0dda5095030a16bceaf56d5ce7e9e3ffa84c3d6
Hevener Ancelmo Pereira <hevener2@hotmail.com>
2022-01-10 09:14:56 -0300

    Aula: Init App

--- cypress/integration/1-getting-started/app-init.spec.js
+++ cypress/integration/1-getting-started/app-init.spec.js
@@ -1,29 +1,9 @@
 describe('App Inicialization', () => {
     it('Displays todos from API on Load', () => {
         cy.server();
-        cy.route('GET', '/ToDoModels', [
-            {
-                "id": 1,
-                "title": "One",
-                "completed": false
-            },
-            {
-                "id": 2,
-                "title": "Two",
-                "completed": false
-            },
-            {
-                "id": 3,
-                "title": "Tree",
-                "completed": false
-            },
-            {
-                "id": 4,
-                "title": "Four",
-                "completed": false
-            },
-        ]);
-
+        cy.fixture('todos').then(todos=>{	
+            cy.route('GET', '/ToDoModels', todos)
+        })
         cy.visit('/');
         cy.get('.task-wrapper').should('have.length', 4);
     });
